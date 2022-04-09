d70c55a7590fc5bb81b0693dfc9be2b6304c3e8e
erxclau <ericlau41802@gmail.com>
2022-04-09 01:41:57 -0400

    Turn off csp-xss assertion

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,10 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     assert: {
-      preset: 'lighthouse:recommended'
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'csp-xss': 'off'
+      }
     }
   },
 };
